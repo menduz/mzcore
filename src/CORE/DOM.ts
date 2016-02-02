331855c18b3fa62c6bdf8c0020c350c1c15cee18
Agustin Mendez (Menduz) <lord.yo.wo@gmail.com>
2016-02-02 11:02:35 -0300

    Removed domParser

--- src/CORE/DOM.ts
+++ src/CORE/DOM.ts
@@ -1,16 +1,10 @@
 namespace mz.core.dom {
-	export var adapter: mz.core.dom.AbstractDomAdapter;
-	export var parser: mz.core.dom.AbstractDomParser;
+    export var adapter: mz.core.dom.AbstractDomAdapter;
 
-	export function setRootDomAdapter(theAdapter: mz.core.dom.AbstractDomAdapter) {
-		if (!adapter) {
-			adapter = theAdapter;
-		}
-	}
-	
-	export function setRootDomParser(theParser: mz.core.dom.AbstractDomParser) {
-		if (!parser) {
-			parser = theParser;
-		}
-	}
+    export function setRootDomAdapter(theAdapter: mz.core.dom.AbstractDomAdapter) {
+        if (!adapter) {
+            mz.scriptBase = theAdapter.getBaseHref() || '';
+            adapter = theAdapter;
+        }
+    }
 }
