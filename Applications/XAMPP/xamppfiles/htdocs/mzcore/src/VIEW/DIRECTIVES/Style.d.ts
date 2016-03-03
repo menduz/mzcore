587eccc32d58b42ac99c590153e277c604195284
Agustin Mendez (Menduz) <lord.yo.wo@gmail.com>
2016-03-03 16:34:11 -0300

    Added bower & pollyfills

--- Applications/XAMPP/xamppfiles/htdocs/mzcore/src/VIEW/DIRECTIVES/Style.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class StyleComponent extends mz.Widget {
-    parent_selector: string;
-    link: mz.Widget;
-    observer: MutationObserver;
-    constructor(root: any, attr: any, children: any, params: any, parent: any, scope: any);
-    textValue: string;
-    textValue_changed(val: any, prevVal: any): void;
-    update(): void;
-}
